4c7e5b7bd0fc9d3bf27f7ce5fd2b3002a6b53e20
Chinnawat-TT <chinnawat.sangrung@gmail.com>
2023-11-04 14:32:17 +0700

    setup for lean tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <>
+      <div className=' bg-red-500'>
+        <p className=' text-white flex items-center justify-center font-bold'> learn - tailwind</p>
+      </div>
+      <div className=" flex items-center  h-full justify-center">
+        <p>su su na kub </p>
+      </div>
+    </>
+  )
+}
+
+export default App
